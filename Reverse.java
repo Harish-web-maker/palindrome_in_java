680ba22bcf1c4d114ed2470b59dceb19bd4106be
Kalluri Harish <harishchowdhary33@gmail.com>
2025-06-24 22:32:03 +0530

    Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,19 +1,32 @@
+// import java.util.*;
+// class Reverse{
+//     public static void main(String[] args){
+//         Scanner obj = new Scanner(System.in);
+//         int n, rev = 0, temp, d;
+//         n = obj.nextInt();             // Taking user input.
+                    
+//         while(n!=0){                 // check the condition.
+//             d = n % 10;              // it's for finding single digit from a large number like 2 digits or more digits. it take's last digit to check and continue the process. 
+//             rev = rev * 10 + d;      // formula for reverse of number.
+//             n = n / 10;              // it's for removing last digit and sends to loop again to continue the processs.
+//         }
+//         System.out.println(rev);
+//     }
+// }
+//to remove all commits just copy what you want and click cntl + /
+
+// o/p:- 23458
+//  85432
+
 import java.util.*;
 class Reverse{
     public static void main(String[] args){
         Scanner obj = new Scanner(System.in);
-        int n, rev = 0, temp, d;
-        n = obj.nextInt();             // Taking user input.
-                    
-        while(n!=0){                 // check the condition.
-            d = n % 10;              // it's for finding single digit from a large number like 2 digits or more digits. it take's last digit to check and continue the process. 
-            rev = rev * 10 + d;      // formula for reverse of number.
-            n = n / 10;              // it's for removing last digit and sends to loop again to continue the processs.
+        String s = obj.nextLine();
+        for(int i = s.length(); i>=0; i--){
+            System.out.println(i);
         }
-        System.out.println(rev);
     }
 }
 
-
-// o/p:- 23458
-//  85432
+// o/p:- 
